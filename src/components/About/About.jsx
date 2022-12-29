6296acd42459b3f2d21ca5431e673c8ff6a300a5
killshotxd <hassanansari211@gmail.com>
2022-12-29 16:46:08 +0530

    up

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,34 +7,37 @@ const About = () => {
       {/* <div  className="">
     <h1 className={`text-center text-white mt-3 ${styles.about}`}>ABOUT US</h1>
     </div> */}
-      <div className="container mt-5 pt-5 d-flex justify-content-around">
-        <div>
-          <div className={styles.migDiv}>
-            <img
-              className={styles.image}
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Johnny_Depp_Jim_Jarmusch_Cannes_1995.jpg/170px-Johnny_Depp_Jim_Jarmusch_Cannes_1995.jpg"
-              alt=""
-            />
+      <div className="container mt-3 pt-5 d-flex justify-content-around">
+        <div className="row">
+          <div className={`col ${styles.colImg} `}>
+            <div className={styles.migDiv}>
+              <img
+                className={styles.image}
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Johnny_Depp_Jim_Jarmusch_Cannes_1995.jpg/170px-Johnny_Depp_Jim_Jarmusch_Cannes_1995.jpg"
+                alt=""
+              />
+            </div>
           </div>
-        </div>
-        <div className="col container">
-          <h1 className={`mb-4 text-white ${styles.mainHeading}`}>
-            JOHNSON'S ENTERTAINMENT
-          </h1>
-          <p className="text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non hic
-            animi modi recusandae distinctio omnis sint, itaque, consectetur
-            molestias laboriosam corporis debitis, eligendi porro eum veritatis
-            magni doloribus cupiditate architecto!
-          </p>
-          <p className="text-white">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. At
-            reprehenderit nulla ducimus earum error odio asperiores, eaque quae
-            sunt blanditiis debitis libero distinctio esse porro dignissimos
-            quisquam voluptatum ut similique recusandae quo ad atque est
-            consequuntur qui. Et nemo aperiam laboriosam consectetur hic,
-            cupiditate, corrupti magni, reiciendis explicabo culpa tempora?
-          </p>
+          <div className="col container d-flex justify-content-center flex-column ">
+            <h1 className={`mb-4 text-black ${styles.mainHeading}`}>
+              JOHNSON'S ENTERTAINMENT
+            </h1>
+            <p className="text-black">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non hic
+              animi modi recusandae distinctio omnis sint, itaque, consectetur
+              molestias laboriosam corporis debitis, eligendi porro eum
+              veritatis magni doloribus cupiditate architecto!
+            </p>
+            <p className="text-black">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. At
+              reprehenderit nulla ducimus earum error odio asperiores, eaque
+              quae sunt blanditiis debitis libero distinctio esse porro
+              dignissimos quisquam voluptatum ut similique recusandae quo ad
+              atque est consequuntur qui. Et nemo aperiam laboriosam consectetur
+              hic, cupiditate, corrupti magni, reiciendis explicabo culpa
+              tempora?
+            </p>
+          </div>
         </div>
       </div>
     </div>
